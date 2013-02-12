76ca6de15e4cffe9458abe1b85fb5fd48334c17c
Raj Bandyopadhyay <plusbzz@gmail.com>
2013-02-11 21:39:02 -0500

    Fix issues with error calculations

--- q1b.m
+++ q1b.m
@@ -1,12 +1,19 @@
 k = 20;
 max_r = 5;
 eta = 0.1;
-lambda = 0.1;
+lambda = 0.2;
 
 % iterate over the files on disk once to find the max values of M and N
 
 [R,N,M] = findMaxIndex("ratings.train.txt",0,0,0);
 [R,N,M] = findMaxIndex("ratings.val.txt",R,N,M);
-[P,Q] = sgd("ratings.train.txt",eta,lambda,k,R,N,M);
 
-%[P,Q] = sgd("test.txt",eta,lambda,k,5,100,100);
+P = rand(N,k)*sqrt(R/k);
+Q = rand(M,k)*sqrt(R/k);
+
+errors = [];
+for i = 1:40
+    [P,Q,E] = sgd("ratings.train.txt",P,Q,eta,lambda);
+    errors = [errors;E];
+end
+errors
--- sgd.m
+++ sgd.m
@@ -1,35 +1,24 @@
-function [P,Q] = sgd(fname,eta,lambda,k,R,N,M)
-    P = rand(N,k)*sqrt(R/k);
-    Q = rand(M,k)*sqrt(R/k);
-
-    % Until convergence
-    %  For each r_iu
-    %   do a step
-    converged = false;  
-    do
-        P_old = P;
-        Q_old = Q;
-        % one iteration
-        fid = fopen (fname);
-        while true
-            [i,u,r] = readRatingLine(fid);
-            if i == -1
-                break;
-            end
-            % if not end of file, do one SGD step
-            p = P(i,1:k);
-            q = Q(u,1:k);
-            eps = r - q*p';
-            P(i,:) = p + eta*(eps*q - lambda*p);
-            Q(u,:) = q + eta*(eps*p - lambda*q);
+function [P,Q,E] = sgd(fname,P_init,Q_init,eta,lambda)
+    P = P_init;
+    Q = Q_init;
+    k = size(Q,2);
+    E = 0;
+    fid = fopen (fname);
+    while true
+        [i,u,r] = readRatingLine(fid);
+        if i == -1
+            break;
         end
-        fclose(fid);
-        % test for convergence
-        errP = norm(P_old-P,"fro");
-        errQ = norm(Q_old-Q,"fro");
-        errP + errQ;
-        if errP + errQ < 0.00005
-            converged = true;
-        end
-    until converged;
+        % if not end of file, do one SGD step
+        p = P(i,1:k);
+        q = Q(u,1:k);
+        eps = r - q*p';
+        p_new = p + eta*(eps*q - lambda*p);
+        q_new = q + eta*(eps*p - lambda*q);
+        P(i,:) = p_new;
+        Q(i,:) = q_new;
+        E = E + (r - q_new*p_new')^2;
+    end
+    E = E + lambda*(norm(P,"fro")^2 + norm(Q,"fro")^2);
+    fclose(fid);       
 end
