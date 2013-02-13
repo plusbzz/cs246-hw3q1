1cb743934f9d5031b3f4358daaf33eb03340d3e6
Raj Bandyopadhyay <plusbzz@gmail.com>
2013-02-12 22:49:01 -0500

    Measure only training error

--- q1b.m
+++ q1b.m
@@ -8,24 +8,15 @@ lambda = 0.2;
 [R,N,M] = findMaxIndex("ratings.val.txt",R,N,M);
 
 errors_tr = [];
-errors_te = [];
-errors_to = [];
 for eta = etas
     E_train = [];
-    E_test  = [];
-    E_to    = [];
     P = rand(N,k)*sqrt(R/k);
     Q = rand(M,k)*sqrt(R/k);
     for i = 1:40
         [P,Q,E_tr] = sgd("ratings.train.txt",P,Q,eta,lambda);
         E_train= [E_train,E_tr];
-        E_te = calculateError("ratings.val.txt",P,Q,lambda); 
-        E_test = [E_test,E_te];
-        E = E_tr + E_te + lambda*(norm(P,"fro")^2 + norm(Q,"fro")^2)
-        E_to = [E_to, E];
+        E = E_tr + lambda*(norm(P,"fro")^2 + norm(Q,"fro")^2);
     end
     errors_tr = [errors_tr;E_train];
-    errors_te = [errors_te;E_test];
-    errors_to = [errors_to;E_to];
 end
-save("results.mat");
+save("results_q1b.mat");
