9116560b80af56aa83d63c2f0fb424da8b19c2ab
Raj Bandyopadhyay <plusbzz@gmail.com>
2013-02-17 22:29:55 -0500

    Fixed calculation of error

--- calculateError.m
+++ calculateError.m
@@ -1,18 +1,22 @@
-function E = calculateError(fname,P,Q,mu,Bu,Bi,lambda)
+function E = calculateError(fname,P,Q,mu,Bu,Bi)
     fid = fopen (fname);
     E = 0;
     k = size(Q,2);
-    while true
-        [i,u,r] = readRatingLine(fid);
-        if i == -1
-            break;
+    bufferSize = 3e4; % scan through the file once
+    buffer = reshape(fscanf(fid, '%d\t%d\t%g', bufferSize),3,[])' ;
+    while ~isempty(buffer)
+        for ix = 1:size(buffer,1)
+            vals = buffer(ix,:);
+            i = vals(1);
+            u = vals(2);
+            r = vals(3);
+            p = P(i,1:k);
+            q = Q(u,1:k);
+            bu = Bu(u,1);
+            bi = Bi(i,1);
+            E = E + (r - mu - bu - bi - q*p')^2;
         end
-        p = P(i,1:k);
-        q = Q(u,1:k);
-        bu = Bu(u,1);
-        bi = Bi(i,1);
-        
-        E = E + (r - mu - bu - bi - q*p')^2;
+        buffer = reshape(fscanf(fid, '%d\t%d\t%g', bufferSize),3,[])' ;
     end
     fclose(fid);       
 end
--- q1d.m
+++ q1d.m
@@ -1,24 +1,25 @@
-%Q1.d.b
-
-k = 20;
-etas = [0.01 0.03];
-lambda = 0.2;
-
 % iterate over the files on disk once to find the max values of M and N
 tic;
 [R,N,M,sum,cnt] = findMaxIndex('ratings.train.txt',0,0,0,0,0);
 [R,N,M,sum,cnt] = findMaxIndex('ratings.val.txt',R,N,M,sum,cnt)
 mu = sum/cnt
 toc
-tic;
-errors1 = [];
-for eta = etas
-    [P,Q,Bu,Bi,E_train,E] = sgd('ratings.train.txt',N,M,R,k,mu,eta,lambda,40);
-    errors1= [errors1; E];
-    E
-end
-errors1
-toc
+
+
+% %Q1.d.b
+% 
+% k = 20;
+% etas = [0.01 0.03];
+% lambda = 0.2;
+% tic;
+% errors1 = [];
+% for eta = etas
+%     [P,Q,Bu,Bi,E_train,E] = sgd('ratings.train.txt',N,M,R,k,mu,eta,lambda,40);
+%     errors1= [errors1; E];
+%     E
+% end
+% errors1
+% toc
 
 %Q1.d.c
 eta = 0.03;
@@ -27,10 +28,11 @@ lambdas = [0.0 0.2];
 errors2= [];
 for lambda = lambdas
     for k = 1:10
+        tic;
         [P,Q,Bu,Bi,E_train,E] = sgd('ratings.train.txt',N,M,R,k,mu,eta,lambda,40);
-        E_tr = calculateError('ratings.train.txt',P,Q,mu,Bu,Bi,lambda); 
-        E_te = calculateError('ratings.val.txt',P,Q,mu,Bu,Bi,lambda);
-        [lambda k E_tr E_te]
+        E_tr = calculateError('ratings.train.txt',P,Q,mu,Bu,Bi); 
+        E_te = calculateError('ratings.val.txt',P,Q,mu,Bu,Bi);
+        [lambda k E_tr E_te toc]
         errors2 = [errors2;lambda k E_tr E_te];
     end
 end
